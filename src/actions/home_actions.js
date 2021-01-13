946672f4dc06b14623652ffdfc5d10f0f80f1869
martondballo <martondballo@gmail.com>
2021-01-12 19:33:31 -0800

    Get products on App render

--- src/actions/home_actions.js
+++ src/actions/home_actions.js
@@ -1,1 +1,14 @@
 export const testAction = () => ({ type: 'TEST_ACTION' });
+
+export const getProducts = () => dispatch => {
+  fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(json => {
+      dispatch(populateProducts(json));
+    });
+};
+
+const populateProducts = products => ({
+  type: `POPULATE_PRODUCTS`,
+  payload: products,
+});
--- src/components/App.react.js
+++ src/components/App.react.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { testAction } from '../actions/home_actions';
+import { testAction, getProducts } from '../actions/home_actions';
 
 function App() {
   const dispatch = useDispatch();
   const store = useSelector(store => store);
+  useEffect(() => dispatch(getProducts()), [dispatch]);
   console.log(store);
   return (
     <div>
