fc547e6299d9f10afaaa9aa50afbe304caa52ebb
martondballo <martondballo@gmail.com>
2021-01-16 17:31:15 -0800

    Display categories

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -2,14 +2,23 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getProducts } from '../actions/products_actions';
 import CategoriesList from './CategoriesList.react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  app: {
+    fontFamily: 'Roboto, Arial, Helvetica, sans-serif',
+  },
+});
 
 function App() {
   const dispatch = useDispatch();
+  const styles = useStyles();
+
   useEffect(() => dispatch(getProducts()), [dispatch]);
   return (
-    <>
+    <div className={styles.app}>
       <CategoriesList />
-    </>
+    </div>
   );
 }
 
