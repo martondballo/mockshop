bc2928da671303a885bd24a76ca6710bdb43ab5e
martondballo <martondballo@gmail.com>
2021-02-06 10:59:50 -0800

    Display product details

--- src/components/Product.react.js
+++ src/components/Product.react.js
@@ -1,20 +1,65 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
 import { useSelector } from 'react-redux';
+import { makeStyles, Typography, Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  productContainer: {
+  centeredFlex: {
     display: 'flex',
     justifyContent: 'center',
+  },
+  productContainer: {
+    maxWidth: '1000px',
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: 16,
+  },
+  productImageContainer: {
+    height: 300,
+    flex: 1,
+    padding: 5,
+    backgroundSize: 'contain',
+    backgroundColor: 'pink',
+  },
+  productDetailsContainer: {
+    flex: 3,
+    paddingLeft: 8,
+  },
+  productDetailRow: {
+    marginBottom: 16,
   },
 });
 
 export default function Product() {
   const styles = useStyles();
+
   const { selectedProductID } = useSelector(state => state.app);
+  const product = useSelector(
+    state => state.products.productsByID[selectedProductID]
+  );
+
+  const { title, description, price } = product;
+
   return (
-    <div className={styles.productContainer}>
-      <h1>{selectedProductID}</h1>
+    <div className={styles.centeredFlex}>
+      <div className={styles.productContainer}>
+        <div className={styles.productImageContainer}></div>
+        <div className={styles.productDetailsContainer}>
+          <div className={styles.productDetailRow}>
+            <Typography variant='h4'>{title}</Typography>
+          </div>
+          <div className={styles.productDetailRow}>
+            <Typography color='textSecondary'>{description}</Typography>
+          </div>
+          <div className={styles.productDetailRow}>
+            <Typography>Price: ${price}</Typography>
+          </div>
+          <div className={styles.productDetailRow}>
+            <Button variant='contained' color='primary'>
+              Add to Cart
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -2,24 +2,30 @@ import { POPULATE_PRODUCTS } from '../actions/productsActions';
 
 const initialState = {
   products: [],
+  productsByID: {},
   productsByCategory: {},
 };
 
 export default function productsReducer(state = initialState, action) {
   switch (action.type) {
     case POPULATE_PRODUCTS:
+      const products = action.payload;
+
       // turns {products: [[product]]} into {[category]: [[product]]}
-      const productsByCategory = action.payload.reduce(
-        (accumulator, product) => {
-          const category = product.category;
-          return {
-            ...accumulator,
-            [category]: [...(accumulator[category] || []), product],
-          };
-        },
-        {}
-      );
-      return { ...state, products: action.payload, productsByCategory };
+      const productsByCategory = products.reduce((accumulator, product) => {
+        const category = product.category;
+        return {
+          ...accumulator,
+          [category]: [...(accumulator[category] || []), product],
+        };
+      }, {});
+
+      // turns {products: [[product]]} into {[productID]: [[product]]}
+      const productsByID = products.reduce((accumulator, product) => {
+        return { ...accumulator, [product.id]: product };
+      }, {});
+
+      return { ...state, products, productsByID, productsByCategory };
 
     default:
       return state;
